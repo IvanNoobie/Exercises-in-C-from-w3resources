78c539fd635a48aecfadc0949949f3caf7cb3b6c
vano9629 <46322473+vano9629@users.noreply.github.com>
2019-01-05 17:58:30 +0300

    Update ex_1.c

--- ex_1.c
+++ ex_1.c
@@ -1,13 +1,6 @@
 /// www.w3resourse.com
 /// C Basic Declarations and Expressions
 /// EXERCISE 1 of 72
-/// TASK:
-///         Write a C program to print your name, date of birth. and mobile number. Go to the editor
-///         Expected Output:
-///
-///         Name   : Alexandra Abramov  
-///         DOB    : July 14, 1975  
-///         Mobile : 99-9999999999
 
 #include <stdio.h>
 int main()
