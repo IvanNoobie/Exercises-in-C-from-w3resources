186fa241d78d3289119b2858a3d155df0fd3e301
vano9629 <46322473+vano9629@users.noreply.github.com>
2019-01-05 17:51:41 +0300

    Update ex_1.c

--- ex_1.c
+++ ex_1.c
@@ -1,13 +1,20 @@
 /// www.w3resourse.com
 /// C Basic Declarations and Expressions
 /// EXERCISE 1 of 72
+/// TASK:
+///         Write a C program to print your name, date of birth. and mobile number. Go to the editor
+///         Expected Output:
+///
+///         Name   : Alexandra Abramov  
+///         DOB    : July 14, 1975  
+///         Mobile : 99-9999999999
 
 #include <stdio.h>
 int main()
 {
-    printf("Name   : Ivan Chuguy\n");
-    printf("DOB    : May 25, 1996\n");
-    printf("Mobile : 8-916-854-39-76\n");
+    printf("Name   : Ivan Ivanov\n");
+    printf("DOB    : June 01, 1995\n");
+    printf("Mobile : 8-999-777-66-55\n");
     
     return 0;
 }
